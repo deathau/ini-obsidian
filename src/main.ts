f600e9d451384c041ba324f57dbf522e18b6d3bb
T. H. Wright <thwright@redemption.studio>
2021-07-08 09:20:40 -0400

    Add xml and fodt.

--- src/main.ts
+++ src/main.ts
@@ -13,15 +13,26 @@ export default class IniPlugin extends Plugin {
 
     // register a custom icon
     this.addDocumentIcon("ini");
+    this.addDocumentIcon("xml");
+    this.addDocumentIcon("fodt");
 
     // register the view and extensions
     this.registerView("ini", this.iniViewCreator);
+    this.registerView("xml", this.xmlViewCreator);
+    this.registerView("fodt", this.xmlViewCreator);
+
     this.registerExtensions(["ini"], "ini");
+    this.registerExtensions(["xml"], "xml");
+    this.registerExtensions(["fodt"], "xml");
   }
 
   // function to create the view
   iniViewCreator = (leaf: WorkspaceLeaf) => {
     return new IniView(leaf);
+  }
+
+  xmlViewCreator = (leaf: WorkspaceLeaf) => {
+    return new XmlView(leaf);
   }
 
   // this function used the regular 'document' svg,
@@ -100,6 +111,7 @@ class IniView extends TextFileView {
 
   // confirms this view can accept ini extension
   canAcceptExtension(extension: string) {
+    
     return extension == 'ini';
   }
 
@@ -113,3 +125,83 @@ class IniView extends TextFileView {
     return "document-ini";
   }
 }
+
+// This is the custom view
+class XmlView extends TextFileView {
+
+  // internal code mirror instance
+  codeMirror: CodeMirror.Editor;
+
+  // this.contentEl is not exposed, so cheat a bit.
+  public get extContentEl(): HTMLElement {
+    // @ts-ignore
+    return this.contentEl;
+  }
+
+  // constructor
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+
+    // create code mirror instance
+    this.codeMirror = CodeMirror(this.extContentEl, {
+      theme: "obsidian"
+    });
+    // register the changes event
+    this.codeMirror.on('changes', this.changed);
+  }
+
+  // when the view is resized, refresh CodeMirror (thanks Licat!)
+  onResize() {
+    this.codeMirror.refresh();
+  }
+
+  // called on code mirror changes
+  changed = async (instance: CodeMirror.Editor, changes: CodeMirror.EditorChangeLinkedList[]) => {
+    // request a debounced save in 2 seconds from now
+    this.requestSave();
+  }
+
+  // get the new file contents
+  getViewData = () => {
+    return this.codeMirror.getValue();
+  }
+
+  // set the file contents
+  setViewData = (data: string, clear: boolean) => {
+    if (clear) {
+      this.codeMirror.swapDoc(CodeMirror.Doc(data, "text/xml"))
+    }
+    else {
+      this.codeMirror.setValue(data);
+    }
+  }
+
+  // clear the view content
+  clear = () => {
+    this.codeMirror.setValue('');
+    this.codeMirror.clearHistory();
+  }
+
+  // gets the title of the document
+  getDisplayText() {
+    if (this.file) return this.file.basename;
+    else return "xml (no file)";
+  }
+
+  // confirms this view can accept ini extension
+  canAcceptExtension(extension: string) {
+
+    return extension == 'xml';
+  }
+
+  // the view type name
+  getViewType() {
+    return "xml";
+  }
+
+  // icon for the view
+  getIcon() {
+    return "document-xml";
+  }
+}
+
